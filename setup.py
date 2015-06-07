841b078045d309312b121f24d7cd6fd3db4f5f3f
Jeffrey Boehm <jeffrey.boehm@hdnet.de>
2015-06-07 14:40:43 +0200

    Setup erzeugt

--- setup.py
+++ setup.py
@@ -1,20 +1,32 @@
-# -*- coding: utf-8 -*-
 """
-uh.cx API interface
+Python library for the uh.cx link shortener.
 
 http://uh.cx/
 """
 
 from distutils.core import setup
 
+from setuptools import find_packages
+
+
 setup(
     name='uhcx',
-    version='0.1.0',
+    version='0.2.1',
     author='J. Boehm',
     author_email='jb@uh.cx',
-    packages=['uhcx', 'uhcx.test'],
-    url='http://uh.cx/page/api',
-    license='LICENSE.txt',
-    description='Provide an api for uh.cx link shortening service.',
-    long_description=open('README.md').read()
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    url='http://uh.cx/',
+    license='MIT',
+    description='Library for the uh.cx link shortener.',
+    install_requires=['requests'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
 )
